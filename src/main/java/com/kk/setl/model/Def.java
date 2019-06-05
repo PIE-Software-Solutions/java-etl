801e1d2a5fddb273311c5556e420210694002993
piesoftsol <35703117+piesoftsol@users.noreply.github.com>
2019-06-05 11:38:59 +0530

    Update Def.java

--- src/main/java/com/kk/setl/model/Def.java
+++ src/main/java/com/kk/setl/model/Def.java
@@ -1,21 +1,3 @@
-/**
- * Copyright (c) 2016 Vijay Vijayaram
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
- * and associated documentation files (the "Software"), to deal in the Software without restriction,
- * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies or substantial
- * portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
- * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
- * THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
 package com.kk.setl.model;
 
 import org.codehaus.jackson.annotate.*;
@@ -60,6 +42,8 @@ public class Def {
     private Map<String, Object> additionalProperties = new HashMap<String, Object>();
     @JsonIgnore
     private Path filePath;
+    @JsonProperty("extthread")
+    private Integer extthread;
 
     /**
      *
@@ -238,4 +222,16 @@ public class Def {
     public void setFilePath(Path filePath) {
         this.filePath = filePath;
     }
+
+    @JsonProperty("extthread")
+	public Integer getExtthread() {
+		return extthread;
+	}
+
+	@JsonProperty("extthread")
+	public void setExtthread(Integer extthread) {
+		this.extthread = extthread;
+	}
+    
+    
 }
